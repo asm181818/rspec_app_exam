85136eded61e4a2a4da8473e0748d2f5f8ce4bb1
asm_m <riy_m218@yahoo.co.jp>
2020-06-20 01:11:00 +0900

    Fix: statusをシンボル形式の記述に変更

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -8,7 +8,7 @@ FactoryBot.define do
     deadline { Random.rand(from..to) }
 
     trait 'completion_task' do
-      status { 'done' }
+      status { :done }
       completion_date { Time.current.yesterday }
     end
   end
