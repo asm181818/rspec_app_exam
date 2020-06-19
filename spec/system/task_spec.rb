ab1e437a5f1a6fc64fda46305a732ee5d4cc4df1
asm_m <riy_m218@yahoo.co.jp>
2020-06-19 13:43:13 +0900

    projectのテストデータをletで定義

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -30,9 +30,9 @@ RSpec.describe 'Task', type: :system do
 
   describe 'Task新規作成' do
     context '正常系' do
+      let(:project) { FactoryBot.create(:project) }
       it 'Taskが新規作成されること' do
-        # TODO: ローカル変数ではなく let を使用してください
-        project = FactoryBot.create(:project)
+        # TODO: ローカル変数ではなく let を使用してください --OK
         visit project_tasks_path(project)
         click_link 'New Task'
         fill_in 'Title', with: 'test'
